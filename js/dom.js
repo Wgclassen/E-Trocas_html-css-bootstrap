87613ed72495521ad0f41aaa3b4533fb68a0b69d
Willian Classen <Wgclassen@gmail.com>
2025-07-14 18:57:25 -0300

    First commit

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+const idUsuario = document.getElementById('usuario');
+
+const idCabecalho = document.querySelector('#cabecalho');
+
+const classFormLabel = document.getElementsByClassName('form-label');
+
+
+
